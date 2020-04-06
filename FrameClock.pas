1bdbd31eb754808f67c3229fa48e20dc085eb06c
František Milt <frantisek.milt@gmail.com>
2020-04-06 16:33:44 +0200

    Implemented base functionality

--- FrameClock.pas
+++ FrameClock.pas
@@ -22,7 +22,7 @@ type
     iUiS:   Int64;    // integral microseconds
   end;
 
-  TFrameClock = class(TCustomListObject)
+  TFrameClock = class(TCustomObject)
   protected
     fHighResolution:  Boolean;
     fFrequency:       Int64;          // [Hz]
@@ -32,28 +32,27 @@ type
     fPrevFrameTicks:  TFCFrameTicks;
     fCurrFrameTicks:  TFCFrameTicks;
     fFrameTime:       TFCFrameTime;
+    fStamps:          array of TFCFrameTicks;
+    fStampCount:      Integer;
     Function GetCreationTime: TFCFrameTime; virtual;
-    // list functions
-    //Function GetCapacity: Integer; override;
-    //procedure SetCapacity(Value: Integer); override;
-    //Function GetCount: Integer; override;
-    //procedure SetCount(Value: Integer); override;
-
+    Function GetActualTime: TFCFrameTime; virtual;
+    Function GetCurrentTicks: TFCFrameTicks; virtual;
+    Function GetTicksDiff(A,B: TFCFrameTicks): TFCFrameTicks; virtual;
+    procedure FrameTimeFromTicks(var FrameTime: TFCFrameTime); virtual;
+    procedure InitializeTime; virtual;
     procedure Initialize; virtual;
     procedure Finalize; virtual;
-    procedure FrameTimeFromTicks(var FrameTime: TFCFrameTime); virtual;
   public
     constructor Create;
     destructor Destroy; override;
-    // list functions
-    //Function LowIndex: Integer; override;
-    //Function HighIndex: Integer; override;
-
-    Function FrameTick: TFCFrameTime; virtual;
+    Function TickFrame: TFCFrameTime; virtual; 
     property HighResolution: Boolean read fHighResolution;
     property Frequency: Int64 read fFrequency;
     property Resolution: Int64 read fResolution;
     property FrameCounter: UInt64 read fFrameCounter;
+    property CreationTicks: TFCFrameTicks read fCreationTicks;
+    property PrevFrameTicks: TFCFrameTicks read fPrevFrameTicks;
+    property CurrFrameTicks: TFCFrameTicks read fCurrFrameTicks;
   {
     CreationTime - time from object creation to current frame end.
   }
@@ -62,6 +61,11 @@ type
     FrameTime - time from previous frame end to current frame end.
   }
     property FrameTime: TFCFrameTime read fFrameTime;
+  {
+    ActualTime - time from previous frame end to actual time (moment the
+                 property is accessed).
+  }
+    property ActualTime: TFCFrameTime read GetActualTime;
   end;
 
 implementation
@@ -77,31 +81,6 @@ const
   FC_NANOS_PER_MILLI = 1000000;     // nanoseconds per millisecond
   FC_MILLIS_PER_DAY  = 86400000;    // milliseconds per day
 
-//------------------------------------------------------------------------------  
-
-Function GetCurrentTicks(HighRes: Boolean): TFCFrameTicks;
-begin
-If HighRes then
-  begin
-    If QueryPerformanceCounter(Result) then
-      Result := Result and $7FFFFFFFFFFFFFFF  // mask out sign bit
-    else
-      raise EFCSystemException.CreateFmt('GetCurrentTicks: System error 0x%.8x.',[GetLastError]);
-  end
-else Result := TFCFrameTicks(Trunc(Now * FC_MILLIS_PER_DAY));
-end;
-
-//------------------------------------------------------------------------------
-
-Function GetTicksDiff(A,B: TFCFrameTicks): TFCFrameTicks;
-begin
-If B > A then
-  Result := B - A
-else
-  Result := High(TFCFrameTicks) - A + B + 1{overflow tick};
-end;
-
-//==============================================================================
 //==============================================================================
 
 Function TFrameClock.GetCreationTime: TFCFrameTime;
@@ -112,9 +91,51 @@ end;
 
 //------------------------------------------------------------------------------
 
-procedure TFrameClock.Initialize;
+Function TFrameClock.GetActualTime: TFCFrameTime;
 begin
-// time measurement
+Result.Ticks := GetTicksDiff(fPrevFrameTicks,GetCurrentTicks);
+FrameTimeFromTicks(Result);
+end;
+
+//------------------------------------------------------------------------------
+
+Function TFrameClock.GetCurrentTicks: TFCFrameTicks;
+begin
+If fHighResolution then
+  begin
+    If QueryPerformanceCounter(Result) then
+      Result := Result and $7FFFFFFFFFFFFFFF  // mask out sign bit
+    else
+      raise EFCSystemException.CreateFmt('TFrameClock.GetCurrentTicks: System error 0x%.8x.',[GetLastError]);
+  end
+else Result := TFCFrameTicks(Trunc(Now * FC_MILLIS_PER_DAY));
+end;
+
+//------------------------------------------------------------------------------
+
+Function TFrameClock.GetTicksDiff(A,B: TFCFrameTicks): TFCFrameTicks;
+begin
+If B > A then
+  Result := B - A
+else
+  Result := High(TFCFrameTicks) - A + B + 1{overflow tick};
+end;
+
+//------------------------------------------------------------------------------
+
+procedure TFrameClock.FrameTimeFromTicks(var FrameTime: TFCFrameTime);
+begin
+FrameTime.Sec := FrameTime.Ticks / fFrequency;
+FrameTime.MiS := FrameTime.Sec * FC_MILLIS_PER_SEC;
+FrameTime.UiS := FrameTime.Sec * FC_MICROS_PER_SEC;
+FrameTime.iMiS := Trunc(FrameTime.MiS);
+FrameTime.iUiS := Trunc(FrameTime.UiS);
+end;
+
+//------------------------------------------------------------------------------
+
+procedure TFrameClock.InitializeTime;
+begin
 If QueryPerformanceFrequency(fFrequency) then
   begin
     fHighResolution := True;
@@ -127,9 +148,15 @@ else
     fFrequency := FC_MILLIS_PER_SEC;
     fResolution := FC_NANOS_PER_MILLI;
   end;
-// other things
+end;
+
+//------------------------------------------------------------------------------
+
+procedure TFrameClock.Initialize;
+begin
+InitializeTime;
 fFrameCounter := 0;
-fCreationTicks := GetCurrentTicks(fHighResolution);
+fCreationTicks := GetCurrentTicks;
 fPrevFrameTicks := fCreationTicks;
 fCurrFrameTicks := fCreationTicks;
 FillChar(fFrameTime,SizeOf(TFCFrameTime),0);
@@ -139,17 +166,6 @@ end;
 
 procedure TFrameClock.Finalize;
 begin
-end;
-
-//------------------------------------------------------------------------------
-
-procedure TFrameClock.FrameTimeFromTicks(var FrameTime: TFCFrameTime);
-begin
-FrameTime.Sec := FrameTime.Ticks / fFrequency;
-FrameTime.MiS := FrameTime.Sec * FC_MILLIS_PER_SEC;
-FrameTime.UiS := FrameTime.Sec * FC_MICROS_PER_SEC;
-FrameTime.iMiS := Trunc(FrameTime.MiS);
-FrameTime.iUiS := Trunc(FrameTime.UiS);
 end;
 
 //==============================================================================
@@ -170,10 +186,11 @@ end;
 
 //------------------------------------------------------------------------------
 
-Function TFrameClock.FrameTick: TFCFrameTime;
+Function TFrameClock.TickFrame: TFCFrameTime;
 begin
+Inc(fFrameCounter);
 fPrevFrameTicks := fCurrFrameTicks;
-fCurrFrameTicks := GetCurrentTicks(fHighResolution);
+fCurrFrameTicks := GetCurrentTicks;
 fFrameTime.Ticks := GetTicksDiff(fPrevFrameTicks,fCurrFrameTicks);
 FrameTimeFromTicks(fFrameTime);
 Result := fFrameTime;
